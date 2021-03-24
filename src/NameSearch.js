7e5ba4c5bf7654873eb81f3ab2bf7d05554fdc32
KHCode <hillkri@oregonstate.edu>
2021-03-24 15:04:10 -0700

    added components for searching and displaying

--- src/NameSearch.js
+++ src/NameSearch.js
@@ -1,41 +1,19 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-const CHARACTER_URL = `https://gateway.marvel.com/v1/public/characters?apikey=${process.env.REACT_APP_API_KEY}`;
+const CharacterForm = styled.form`
+    margin: 
+`;
 
-export default function NameSearch() {
-    const [name, setName] = useState('');
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [charObject, setCharObject] = useState({});
-    const characterNameRef = useRef();  
-    function handleSearch(e){
-        e.preventDefault();
-        setName(characterNameRef.current.value);
-        alert(name);
-        characterNameRef.current.value = '';
-    }
-    useEffect(() => {
-        if(name === '') return;
-        fetch(`${CHARACTER_URL}&name=${name}`)
-            .then(res => res.json())
-            .then(
-            (result) => {
-                setIsLoaded(true);
-                setCharObject(result.data.results);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-            )
-    }, [name])
-
+export default function NameSearch (props) {
     return (
-        <div>
-            <input type="text" ref={characterNameRef} placeholder="enter character name"></input>
-            <button type="submit" onClick={handleSearch}>Find Character</button>
-            <div>{charObject.name}</div>
-        </div>
+        <form className="character-search" onSubmit={props.handleSubmit(props.onSubmit)}>
+            <h1>Find a Character</h1>
+
+            <label>Name:</label>
+            <input type="text" name="characterName" placeholder="enter character name" ref={props.register} />
+            <input type="submit" />
+        </form>
         
     );
 }
